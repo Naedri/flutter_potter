c146511d7c09a863870a547afebfcab852902d8c
Vincent Escoffier <vincent.escoffier1@gmail.com>
2023-06-05 15:06:30 +0200

    using riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
-// main.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:henri_pottier_flutter/screens/book_detail_screen.dart';
-import 'package:henri_pottier_flutter/screens/home_screen.dart';
 
-void main() => runApp(const MyApp());
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Book Shopping App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Book Shopping App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const HomeScreen(),
+          BookDetailScreen.routeName: (context) => const BookDetailScreen(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (ctx) => HomeScreen(),
-        BookDetailScreen.routeName: (ctx) => const BookDetailScreen(),
-      },    );
+    );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,61 +1,31 @@
-// screens/home_screen.dart
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:henri_pottier_flutter/models/book.dart';
 import 'package:henri_pottier_flutter/screens/book_detail_screen.dart';
 import 'package:http/http.dart' as http;
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+final booksProvider = FutureProvider<List<Book>>((ref) async {
+  final response =
+      await http.get(Uri.parse('https://henri-potier.techx.fr/books'));
+  final jsonData = json.decode(response.body);
 
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<Book> books = [];
-  bool isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    fetchBooks();
+  List<Book> fetchedBooks = [];
+  for (var item in jsonData) {
+    var price = item["price"];
+    var synopsis = item["synopsis"] as List<dynamic>;
+    var cover = item["cover"];
+    var title = item["title"];
+    fetchedBooks.add(
+        Book(title, synopsis.first, cover, double.parse(price.toString())));
   }
 
-  Future<void> fetchBooks() async {
-    setState(() {
-      isLoading = true;
-    });
+  return fetchedBooks;
+});
 
-    try {
-      final response = await http.get(Uri.parse('https://henri-potier.techx.fr/books'));
-      final jsonData = json.decode(response.body);
-
-      List<Book> fetchedBooks = [];
-      for (var item in jsonData) {
-        var price = item["price"];
-        var synopsis = item["synopsis"] as List<dynamic>;
-        var cover = item["cover"];
-        var title = item["title"];
-        fetchedBooks.add(Book(title, synopsis.first, cover, double.parse(price.toString())));
-      }
-
-      setState(() {
-        books = fetchedBooks;
-      });
-    } catch (error) {
-      // Handle error
-      if (kDebugMode) {
-        print('Error fetching books: $error');
-      }
-    } finally {
-      setState(() {
-        isLoading = false;
-      });
-    }
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -63,26 +33,37 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Book Shopping App'),
       ),
-      body: isLoading
-          ? const Center(
+      body: Consumer(
+        builder: (context, widgetRef, _) {
+          final booksAsyncValue = widgetRef.watch(booksProvider);
+          return booksAsyncValue.when(
+            data: (books) {
+              return ListView.builder(
+                itemCount: books.length,
+                itemBuilder: (ctx, index) {
+                  return ListTile(
+                    leading: Image.network(books[index].cover),
+                    title: Text(books[index].title),
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        BookDetailScreen.routeName,
+                        arguments: books[index],
+                      );
+                    },
+                  );
+                },
+              );
+            },
+            loading: () => const Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: books.length,
-              itemBuilder: (ctx, index) {
-                return ListTile(
-                  leading: Image.network(books[index].cover),
-                  title: Text(books[index].title),
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context,
-                      BookDetailScreen.routeName,
-                      arguments: books[index],
-                    );
-                  }
-                );
-              },
             ),
+            error: (error, stackTrace) => Center(
+              child: Text('Error: $error'),
+            ),
+          );
+        },
+      ),
     );
   }
 }
